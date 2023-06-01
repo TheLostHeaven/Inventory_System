5d14e86f52f3ee98bbb2065b62997e725f82cb05
Daniel <dannyking07@outlook.es>
2023-06-01 14:30:41 -0500

    update: [main] se declararon varias modificacione en la sintaxis

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from fastapi.responses import HTMLResponse
 from config.database import engine,Base
 
 from middlewares.error_handler import Errorhandler
-from routers.movie import movie_router
-from routers.genres import genres_router
+# from routers.movie import movie_router
+# from routers.genres import genres_router
 
 
 
@@ -15,8 +15,9 @@ app.title = "Mi app con FastAPI"
 app.version = "0.0.1"
 
 app.add_middleware(Errorhandler)
-app.include_router(movie_router)
-app.include_router(genres_router)
+
+# app.include_router(movie_router)
+# app.include_router(genres_router)
 
 
 Base.metadata.create_all(bind=engine)
