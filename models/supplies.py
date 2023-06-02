172b7f3aa36071911f55e65ae842d0b31323aa63
Daniel <dannyking07@outlook.es>
2023-06-02 09:49:19 -0500

    changes: [ Models -> supplies]

--- models/supplies.py
+++ models/supplies.py
@@ -1,10 +1,18 @@
-from sqlalchemy import Column, ForeignKey, Integer, Float
+from sqlalchemy import Column, Integer, Float, ForeignKey
 from config.database import Base
+from sqlalchemy.orm import relationship
 
 class Supplies(Base):
+
+
     __tablename__ ="supplies"
 
+
     id = Column(Integer, primary_key = True)
-    product_id = Column(Integer, ForeignKey={"product.id"})
-    supplier_id = Column(Integer, ForeignKey={"supplier.id"})
+    product_id = Column(Integer)
+    supplier_id = Column(Integer )
     purchase_price = Column(Float)
+
+
+    #product = relationship("Product", back_populates="supplies")
+    #supplier = relationship("Supplier", back_populates="supplies")
