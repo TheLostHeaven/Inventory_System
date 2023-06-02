f21e180c6631c6360e8328128d175e2630472c64
Daniel <dannyking07@outlook.es>
2023-06-01 22:11:56 -0500

    changes: [ Main ]

--- main.py
+++ main.py
@@ -5,9 +5,8 @@ from fastapi.responses import HTMLResponse
 from config.database import engine,Base
 
 from middlewares.error_handler import Errorhandler
-# from routers.movie import movie_router
-# from routers.genres import genres_router
 
+from routers.supplies import supplies_router
 
 
 app = FastAPI()
@@ -16,8 +15,8 @@ app.version = "0.0.1"
 
 app.add_middleware(Errorhandler)
 
-# app.include_router(movie_router)
-# app.include_router(genres_router)
+
+app.include_router(supplies_router)
 
 
 Base.metadata.create_all(bind=engine)
