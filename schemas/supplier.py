3eb2a08fd1e75cd524412771ba65135db47f3303
Daniel <dannyking07@outlook.es>
2023-06-04 14:38:55 -0500

    feat [schemas -> supplier]

---
+++ schemas/supplier.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel, Field
+
+from typing import Optional
+
+
+class Supplier (BaseModel):
+    id : Optional[int]= None
+    sub_name : str = Field(max_length=40,min_length=2,description="name supplier")
+    address : str = Field(max_length=40,min_length=2,description="fitting room address")
+    phone : int = Field(ge=5)
+    email : str = Field(max_length=40,min_length=2,description="provider email")
+    
+    class config:
+        schemas_extra = {
+            "example":{
+                "id":1,
+                "sub_name":"Distribuidor gerenrico",
+                "address":"cualquiera",
+                "phone":3158000000,
+                "email":"example@gmail.com"            
+            }
+        }
