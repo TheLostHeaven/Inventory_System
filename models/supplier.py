d96698af0aa03825a35e37683d8c50474577c77e
Daniel <dannyking07@outlook.es>
2023-06-04 14:37:59 -0500

    feat [models -> supplier]

--- models/supplier.py
+++ models/supplier.py
@@ -1,16 +1,14 @@
-from sqlalchemy import Column,Integer, String, Float
+from sqlalchemy import Column, Integer, String
+
 from config.database import Base
-from sqlalchemy.orm import relationship
 
 class Supplier(Base):
     
     __tablename__ = "supplier"
-
-
+    
     id = Column(Integer,primary_key=True)
-    sud_name = Column(String)
+    sub_name = Column(String)
     address = Column(String)
     phone = Column(Integer)
-    email = Column(String)
+    email = Column(String) 
 
-    #supplies = relationship("Supplies", foreign_keys="Supplies.suplier.id", back_populates="supplier")
