956c1a80b5e20ab7395d6adc7610336552bb008c
Daniel <dannyking07@outlook.es>
2023-06-04 15:36:21 -0500

    feat [routers -> supplies]

--- routers/supplies.py
+++ routers/supplies.py
@@ -1,47 +1,47 @@
-from fastapi import APIRouter,Path, Query
+from fastapi import APIRouter,Path,Query
 from fastapi.responses import HTMLResponse, JSONResponse
-from fastapi.encoders import jsonable_encoder
+from fastapi.encoders import jsonable_encoder 
+
 
 from service.supplies import SuppliesService
-# from schemas.supplies import Supplies
+from schemas.supplies import Supplies
 from config.database import Session
+
 
 supplies_router = APIRouter()
 
-#All get for swagger
-@supplies_router.get('/actor',tags=['actor'],status_code=200)
+@supplies_router.get('/supplies', tags = ['supplies'], status_code=200)
 def get_supplies():
     db = Session()
     result = SuppliesService(db).get_supplies()
-    return JSONResponse (content=jsonable_encoder(result), status_code=200)
+    return JSONResponse(content=jsonable_encoder(result), status_code= 200)
 
-#
-@supplies_router.get('/actor_for_id',tags=['actor'],status_code=200)
-def get_for_id(id:int):
+@supplies_router.get('/supplies_for_id', tags = ['supplies'], status_code=200)
+def get_supplies_for_id(id:int):
     db = Session()
-    result = ActorService(db).get_for_id(id)
-    return JSONResponse (content=jsonable_encoder(result), status_code=200)
+    result = SuppliesService(db).get_for_id(id)
+    return JSONResponse(content=jsonable_encoder(result), status_code= 200)
 
-@actor_router.post('/actor',tags=['actor'],status_code=201)
-def create_actor(actor:Actor):
+@supplies_router.post('/supplies', tags=['supplies'], status_code= 200)
+def create_supplies(supplies:Supplies):
     db = Session()
-    ActorService(db).create_actor(actor)
-    return JSONResponse (content={"menssage": "acotr created succesfully", "status_code": 201}, status_code= 201)
+    SuppliesService(db).create_supplies(supplies)
+    return JSONResponse(content={"message":"supplies created", 'status_code': 200})
 
-@actor_router.put('/actor{id}', tags=['actor'])
-def update_actor(id:int,data:Actor):
+@supplies_router.put('/supplies{id}', tags=['supplies'])
+def update_supplies(id: int, data: Supplies):
     db = Session()
-    result = ActorService(db).get_for_id(id)
+    result = SuppliesService(db).get_for_id(id)
     if not result:
-        return JSONResponse(content= {"message" : "actor don't found", "status_code":404})
-    ActorService(db).update_actor(data)
-    return JSONResponse(content={"message":"actor updated succesfully", "status_code":200}, status_code=200)
+        return JSONResponse(content={"message": "supplies not found", "status_code": 400})
+    SuppliesService(db).update_supplies(id,data)
+    return JSONResponse(content={"message": "supplies updated successfully", "status_code": 200})
 
-@actor_router.delete('/actor{id}',tags=['actor'])
-def delete_actor(id:int, data:Actor):
+@supplies_router.delete('/supplies{id}', tags=['supplies'])
+def delete_supplies(id:int):
     db = Session()
-    result = ActorService(db).get_for_id(id)
+    result = SuppliesService(db).get_for_id(id)
     if not result:
-        return JSONResponse(content= {"message":"actor don't found", "status_code":404})
-    ActorService(db).update_actor(data)
-    return JSONResponse(content={"message":"actor delete succesfully", "status_code":200}, status_code=200)
+        return JSONResponse(content= {"message":"supplies don't found", "status_code": 404})
+    SuppliesService(db).delete_supplies(id)
+    return JSONResponse(content={"message":"supplies update succesfully","status_code": 200})
